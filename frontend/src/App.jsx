390b5ca4122960b6da48b352419ba4fe67da631c
Mike Dresser <116307817+michaeldresser81@users.noreply.github.com>
2024-04-26 12:23:56 -0400

    Add logo

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,7 +2,14 @@ import { Outlet } from 'react-router-dom';
 import { useEffect, useState } from 'react';
 
 function App() {
-  return <Outlet />;
+  return (
+    <>
+      <div id="header">
+        <h1 className="logo">Menu Binder</h1>
+      </div>
+      <Outlet />
+    </>
+  );
 }
 
 export default App;
