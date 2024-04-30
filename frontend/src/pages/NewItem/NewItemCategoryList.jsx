82853523f4b0325a860c5ceedeaac085ad9fd363
Mike Dresser <116307817+michaeldresser81@users.noreply.github.com>
2024-04-30 15:07:00 -0400

    Create menu category select list

--- frontend/src/pages/NewItem/NewItemCategoryList.jsx
+++ frontend/src/pages/NewItem/NewItemCategoryList.jsx
@@ -1,9 +1,30 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 function NewItemCategoryList() {
+  const [allMenus, setAllMenus] = useState([]);
+  useEffect(() => {
+    fetch(`http://127.0.0.1:5555/menus`)
+      .then((res) => res.json())
+      .then((data) => {
+        setAllMenus(data);
+      });
+  }, []);
   return (
     <select>
       <option value="">Select menu category</option>
+      {allMenus.map((menu) => {
+        return (
+          <optgroup label={menu.name}>
+            {menu.categories.map((category) => {
+              return (
+                <option value={category.id}>
+                  {category.name || menu.name}
+                </option>
+              );
+            })}
+          </optgroup>
+        );
+      })}
     </select>
   );
 }
